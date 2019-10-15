1d7fdce751a366b45e397c8da42a9c741e3725ca
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-15 14:51:59 +0100

    delete friend stretch completed

--- friends-app/src/components/FriendsList.js
+++ friends-app/src/components/FriendsList.js
@@ -1,9 +1,25 @@
 import React, { useRef, useState, useEffect } from 'react';
 import withAuth from './axios';
+import axios from 'axios';
+
+const token = localStorage.getItem('token');
 
 export default function Login(props) {
   const [friends, setFriends] = useState([]);
 
+  const Delete = (id) => {
+    axios({
+      method: 'delete',
+      url: `http://localhost:5000/api/friends/${id}`,
+      data: {},
+      headers: {
+        'Content-Type': 'application/json',
+         authorization: token,
+      },
+    }).then((res) => {
+      setFriends(res.data);
+    }).catch(e => console.log(e));
+  }
   useEffect(() => {
     // We need the wrapped axios instead, to send token
     // along automatically, in an Authorization header
@@ -27,6 +43,7 @@ export default function Login(props) {
               <h1>ID: {friend.id}</h1>
               <h1>Name: {friend.name}</h1>
               <h1>Email: {friend.email}</h1>
+              <button onClick={() => Delete(friend.id)}>Delete</button>
             </div>
           )) : 'No Friends in your List' 
       }
